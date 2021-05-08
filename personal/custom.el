d0923b6cd98933a75aabfbf68e2290e7952538d7
David Ziegler <dz@entropy.org>
2021-05-08 16:42:16 -0400

    .

---
+++ personal/custom.el
@@ -0,0 +1,42 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(avy-timeout-seconds 0.5)
+ '(fringe-mode 0 nil (fringe))
+ '(indent-tabs-mode nil)
+ '(lsp-headerline-breadcrumb-enable nil)
+ '(lsp-keymap-prefix "C-p")
+ '(lsp-rust-analyzer-call-info-full nil)
+ '(lsp-rust-analyzer-completion-add-call-parenthesis nil)
+ '(lsp-rust-analyzer-completion-postfix-enable nil)
+ '(lsp-rust-analyzer-diagnostics-enable nil)
+ '(lsp-rust-clippy-preference "off")
+ '(lsp-rust-racer-completion nil)
+ '(lsp-rust-server 'rust-analyzer)
+ '(lsp-ui-doc-enable nil)
+ '(lsp-ui-peek-enable nil)
+ '(lsp-ui-sideline-enable nil)
+ '(org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)
+     (sqlite . t)
+     (js . t)
+     (sql . t)
+     (shell . t)
+     (calc . t)))
+ '(org-confirm-babel-evaluate nil)
+ '(org-startup-folded "showall")
+ '(package-selected-packages
+   '(kotlin-mode zig-mode d-mode protobuf-mode framemove some-package yaml-mode web-mode tide ron-mode flycheck-rust cargo rust-mode company-anaconda anaconda-mode lsp-ui lsp-mode json-mode js2-mode gotest go-projectile go-eldoc company-go go-mode rainbow-mode elisp-slime-nav rainbow-delimiters key-chord company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep projectile operate-on-number nlinum move-text magit imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region editorconfig easy-kill discover-my-major diminish diff-hl crux browse-kill-ring anzu ag ace-window))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(typescript-indent-level 2)
+ '(undo-tree-visualizer-diff t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:inherit nil :extend t)))))
