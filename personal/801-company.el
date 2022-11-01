9dcc01e783d1123abb6ed629e9b3cce0d2f47b85
David Ziegler <dz@entropy.org>
2022-11-01 09:05:14 -0300

    .

---
+++ personal/801-company.el
@@ -0,0 +1,7 @@
+;; as per https://emacs.stackexchange.com/questions/13286/how-can-i-stop-the-enter-key-from-triggering-a-completion-in-company-mode
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "<return>") nil)
+  (define-key company-active-map (kbd "RET") nil)
+  (define-key company-active-map (kbd "C-SPC") #'company-complete-selection))
+
