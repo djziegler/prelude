dcd03d87fc72670c03851f4d415126b4555b6022
David Ziegler <dz@entropy.org>
2023-01-09 14:35:43 -0500

    .

--- personal/custom.el
+++ personal/custom.el
@@ -3,56 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(auto-save-default t)
- '(auto-save-file-name-transforms '((".*" "~/.emacs-saves" t)))
- '(avy-timeout-seconds 0.5)
- '(cargo-process--command-current-test "test")
- '(cargo-process--enable-rust-backtrace t)
- '(custom-enabled-themes '(oled-dark))
- '(custom-safe-themes
-   '("395ad51224d80bb014db3c9f0d7ec872cc1620f7299f69b349c6c937c8542919" "570eeea1d7a691ffbfb8dafd2a31e6291d54f3743147290856b58c247095e076" "39a27aefcbe8e98896b238072f582b50349d52810eb936191ba543289c6665b7" "8b624f85e349d252904712df59c8c5cf392c129b0a1e81d3e1ca72291dd0c5e1" "db9a19afa4794ed518e213968b76354b8ba6942fe7a461b83943c0ae5e2f77b1" "65691fffa86cc30f7123e751c106682405885ef788c0d49194053e83a2967428" "29a9a486d9cf67333bc37b64578b4a1496a0d9e6597e485c0913b6a2b2b11fd4" "3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" default))
- '(fringe-mode 0 nil (fringe))
- '(indent-tabs-mode nil)
- '(ivy-use-virtual-buffers 'recentf)
- '(lsp-file-watch-threshold 50000)
- '(lsp-headerline-breadcrumb-enable nil)
- '(lsp-rust-analyzer-call-info-full t)
- '(lsp-rust-analyzer-completion-add-call-parenthesis nil)
- '(lsp-rust-analyzer-completion-postfix-enable t)
- '(lsp-rust-analyzer-diagnostics-enable nil)
- '(lsp-rust-clippy-preference "off")
- '(lsp-rust-racer-completion t)
- '(lsp-rust-server 'rust-analyzer)
- '(lsp-ui-doc-delay 1.5)
- '(lsp-ui-doc-enable nil)
- '(lsp-ui-peek-always-show t)
- '(lsp-ui-peek-list-width 15)
- '(lsp-ui-sideline-show-code-actions f)
- '(org-babel-load-languages
-   '((emacs-lisp . t)
-     (python . t)
-     (sqlite . t)
-     (js . t)
-     (sql . t)
-     (shell . t)
-     (calc . t)))
- '(org-confirm-babel-evaluate nil)
- '(org-startup-folded "showall")
  '(package-selected-packages
-   '(tuareg back-button cython-mode csv-mode doom-modeline use-package doom-modeline-mode scss-mode kotlin-mode zig-mode d-mode protobuf-mode framemove some-package yaml-mode web-mode tide ron-mode flycheck-rust cargo rust-mode company-anaconda anaconda-mode lsp-ui lsp-mode json-mode js2-mode gotest go-projectile go-eldoc company-go go-mode rainbow-mode elisp-slime-nav rainbow-delimiters key-chord company counsel swiper ivy exec-path-from-shell zop-to-char oled-dark-theme which-key volatile-highlights undo-tree super-save smartrep projectile operate-on-number nlinum move-text magit imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region editorconfig easy-kill discover-my-major diminish diff-hl crux browse-kill-ring anzu ag ace-window))
- '(prelude-theme 'oled-dark)
- '(scroll-bar-mode nil)
- '(sp-autoinsert-pair nil)
- '(tool-bar-mode nil)
- '(typescript-indent-level 2)
- '(undo-tree-auto-save-history nil)
- '(undo-tree-visualizer-diff t))
+   '(doom-modeline use-package lsp-ivy zig-mode yaml-mode web-mode tide scss-mode ron-mode yasnippet tree-sitter-langs tree-sitter flycheck-rust cargo rust-mode company-anaconda anaconda-mode json-mode js2-mode gotest go-projectile go-mode lsp-ui lsp-mode rainbow-mode elisp-slime-nav rainbow-delimiters key-chord company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t (:background "grey10"))))
- '(hl-line ((t (:inherit nil :extend t)))))
+ )
